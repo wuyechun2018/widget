28ed4e7de3ef351880536aa6378fafd2b573bc7e
wuyechun <wuyechun2018@163.com>
2016-07-22 17:38:38 +0800

    project init

---
+++ widget-excel/src/main/java/org/widget/excel/App.java
@@ -0,0 +1,13 @@
+package org.widget.excel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
