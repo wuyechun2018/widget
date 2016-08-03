dde777932d63e386c48619a8516ce8ea53d6d9d9
wuyechun <wuyechun2018@163.com>
2016-08-03 20:12:38 +0800

    widget web become a spring web

--- widget-excel/src/main/java/org/widget/excel/App.java
+++ widget-excel/src/main/java/org/widget/excel/App.java
@@ -1,13 +1,16 @@
 package org.widget.excel;
 
 /**
- * Hello world!
- *
+ * 
+ * ClassName: App Function: TODO ADD FUNCTION. Reason: TODO ADD REASON(可选).
+ * date: 2016-7-26 上午8:47:12
+ * 
+ * @author wuyechun
+ * @version
+ * @since JDK 1.7
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
